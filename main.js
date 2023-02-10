90fce2ee7fb780b5e9634489019639aebfb7a8e9
ngzdllo <ngzdllo@gmail.com>
2023-02-10 08:55:58 -0500

    se crea ejemplo1

---
+++ main.js
@@ -0,0 +1,15 @@
+var jugador="James Rodriguez"
+
+if(true){
+    var jugador="Lucho Diaz"
+}
+
+const VALOR_IVA=0.19 
+
+console.log("hola "+jugador)
+console.log(`hola ${jugador}`)
+
+
+//Constante en mayuscula
+//let = local 
+//var = global 
